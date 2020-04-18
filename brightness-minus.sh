1d6f6f6b2348c3ee1614dfa7ad1a494ea749e7f9
Pavan <pavan.gayakwad@gmail.com>
2020-04-18 19:43:04 +0530

    Increase decrease brightness automatically
    These scripts extract brightness value from monitor and then increase or decrease brightness by step value 0.05. 
    
    Assign shortcut to these scripts from Gnome keyboard shortcut settings and use them without manually entering brightness values

---
+++ brightness-minus.sh
@@ -0,0 +1,6 @@
+bg=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+tune=`echo "scale=2; $bg - 0.05" | tr , . | bc`
+echo $tune
+xrandr --output HDMI-1 --brightness $tune
+xrandr --output DP-1 --brightness $tune
+
