7ccf4df5ef7180b77f79c5876aee7e34e067be55
Pavan <pavan.gayakwad@gmail.com>
2020-05-04 17:00:40 +0530

    Added sync command
    “sync” in a Terminal which won’t return/complete until all disk writes everywhere are finished.

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,5 @@
-umount /media/pavan/dreams3
+sync #this will ensure all pending writes to disk are completed.
+umount /media/pavan/dreams
 if [ $? -eq 0 ]; then
 	sure=$(zenity --entry --text "sure? press y to continue")
 	if [ $sure = y ]; then 
