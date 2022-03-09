22541a1ee6c84ab35c32a8f4961f18a5b97c1057
Pavan <pavan.gayakwad@gmail.com>
2022-03-10 00:00:49 +0530

    Key binding script for linux gnome

---
+++ key-binding.bat
@@ -0,0 +1,32 @@
+gsettings set org.gnome.mutter dynamic-workspaces true
+#gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+gsettings set org.gnome.shell.keybindings switch-to-application-1 [] 
+gsettings set org.gnome.shell.keybindings switch-to-application-2 [] 
+gsettings set org.gnome.shell.keybindings switch-to-application-3 [] 
+gsettings set org.gnome.shell.keybindings switch-to-application-4 [] 
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']" 
+ 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']" 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']" 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']" 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>4']" 
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Primary><Super>left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Primary><Super>down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Primary><Super>up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Primary><Super>right']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Primary><Super><Shift>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Primary><Super><Shift>Right']"
+
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>Down']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+
+gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>c']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
