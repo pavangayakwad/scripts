39667d2d06d36d97557cb9118d41192d44f7fe87
Pavan <pavan.gayakwad@gmail.com>
2023-09-23 01:55:11 +0530

    Update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -169,6 +169,7 @@
 	dotnet-sdk_7
 	gparted
 	util-linux
+	bcompare
 	#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
