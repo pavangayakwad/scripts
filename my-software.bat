89d5be6f6f22ba1de21d5a4b7df23c21a70b16fd
Pavan <pavan.gayakwad@gmail.com>
2024-06-29 00:09:37 +0530

    win-get software setup for windows

---
+++ my-software.bat
@@ -0,0 +1,23 @@
+powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+winget install ^
+notepad++ ^
+startallback ^
+SlackTechnologies.Slack ^
+Microsoft.teams ^
+Microsoft.skype ^
+7zip.7zip ^
+eMClient.eMClient ^
+Oracle.MySQLWorkbench ^
+MongoDB.Compass.Full ^
+PostgreSQL.pgAdmin ^
+SumatraPDF.SumatraPDF ^
+Python.Python.3.11 ^
+Mozilla.Firefox ^
+OpenVPNTechnologies.OpenVPNConnect ^
+SonicWALL.NetExtender ^
+TeamViewer.TeamViewer ^
+Opera.Opera ^
+Postman.Postman ^
+flameshot ^
+hluk.CopyQ ^
